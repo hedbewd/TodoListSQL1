a28ff6aeae8c7a01feea0b49067a5574d031436c
hedbewd <hedbewd@gmail.com>
2021-09-22 22:58:18 +0900

    Second Commit

--- src/com/todo/dao/TodoItem.java
+++ src/com/todo/dao/TodoItem.java
@@ -1,17 +1,26 @@
 package com.todo.dao;
 
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class TodoItem {
     private String title;
     private String desc;
-    private Date current_date;
+    private String current_date;
 
-
-    public TodoItem(String title, String desc){
-        this.title=title;
-        this.desc=desc;
-        this.current_date=new Date();
+	public TodoItem(String title, String desc){
+        this.title = title;
+        this.desc = desc;
+        
+        Date now = new Date();
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        this.current_date = format1.format(now);
+    }
+	
+	public TodoItem(String title, String desc, String current_date){
+        this.title = title;
+        this.desc = desc;
+        this.current_date = current_date;
     }
     
     public String getTitle() {
@@ -30,11 +39,15 @@ public class TodoItem {
         this.desc = desc;
     }
 
-    public Date getCurrent_date() {
+    public String getCurrent_date() {
         return current_date;
     }
 
-    public void setCurrent_date(Date current_date) {
+    public void setCurrent_date(String current_date) {
         this.current_date = current_date;
+    }
+    
+    public String toSaveString() {
+    	return title + "##" + desc + "##" + current_date + "\n";
     }
 }
